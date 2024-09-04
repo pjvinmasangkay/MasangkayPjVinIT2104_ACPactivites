4988d4faae59d58d8452f755705b12c4ec787aa7
pjvinmasangkay <23-00190@g.batstate-u.edu.ph>
2024-09-04 12:26:32 +0800

    Update lab1_item2.py

--- Laboratory Activity 1 - 04 Sept 2024/lab1_item2.py
+++ Laboratory Activity 1 - 04 Sept 2024/lab1_item2.py
@@ -1,3 +1,6 @@
 character = input ("Enter two space separated characters: ").split()
 print ("----------------")
 print ("The character with the greater value is: " + max(character))
+print ("Showing ASCII values: ")
+print(str(character[0])+ ":" + str(ord(character[0])))
+print(str(character[1])+ ":" + str(ord(character[1])))
