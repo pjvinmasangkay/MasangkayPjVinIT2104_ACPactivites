7441750358a51e1ff5fc80186a5ebed1fd0793b1
pjvinmasangkay <23-00190@g.batstate-u.edu.ph>
2024-09-11 12:54:29 +0800

    set_operations.py

---
+++ Exercise Sets/set_operations.py
@@ -0,0 +1,8 @@
+set1 = {8, 16, 24, 32, 44}
+set2 = {7, 14, 8, 32, 21}
+
+print("set1 - set2 difference:", set1 - set2)
+print("set2 - set1 difference:", set2 - set1)
+print("set1 | set2 union sets:", set1 | set2)
+print("set1 ^ set2 symmetric difference:", set1 ^ set2)
+print("set1 & set2 intersection:", set1 & set2)
