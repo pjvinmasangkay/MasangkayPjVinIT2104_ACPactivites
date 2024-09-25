740d5635299c9154e1c93ff6a80cfc39476f9a35
pjvinmasangkay <23-00190@g.batstate-u.edu.ph>
2024-09-25 11:39:05 +0800

    Update lab2_item1.py

--- Laboratory Activity 2 - 25 Sept 2024/lab2_item1.py
+++ Laboratory Activity 2 - 25 Sept 2024/lab2_item1.py
@@ -1,8 +1,7 @@
-x = input("Enter an integer: ")
+x = int(input("Enter an integer: "))
 
-if x.isdigit() or (x[0] == '-' and x[1:].isdigit()):
-    num = x
-    if num == num[::-1]:
-        print("Palindrome")
-    else:
-        print("Not a Palindrome")
+num = str(x)
+if num == num[::-1]:
+    print("Palindrome")
+else:
+    print("Not a Palindrome")
